0244579073df55a561ed0ff8773146b6e4b972b6
cutymeoRBLX <hkhanhnam81@gmail.com>
2023-06-12 17:50:55 +0700

    Pushed to GitHub.

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+import Backend from "./Backend";
+import ServerFrontend from "./ServerFrontend";
+import DiscordBot from "./DiscordBot";
+
+if (fs.existsSync(path.resolve(__dirname, "../dev_config/.env"))) {
+	dotenv.config({ path: path.resolve(__dirname, "../dev_config/.env") });
+}
+
+const RobloxToken: string | undefined = process.env["LBCookie"];
+const BotToken: string | undefined = process.env["BotToken"];
+const PrivilegeApiKey: string = process.env["PrivilegeApiKey"] || "LB_DEVTEST_PRIVILEGE_API_HOLDER_ABC_XYZ";
+
+const WhitelistBackend = new Backend(RobloxToken, PrivilegeApiKey);
+const Bot = new DiscordBot(WhitelistBackend, ";", BotToken);
+new ServerFrontend(WhitelistBackend, Bot);
+Bot.start();
