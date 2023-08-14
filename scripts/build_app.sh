f49e208ee8f78bc2cfa6da07899082733d6eed59
cutymeoRBLX <hkhanhnam81@gmail.com>
2023-08-14 16:21:51 +0700

    It didn't copied dbs :/

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -1,6 +1,8 @@
 mkdir -p app/js
 mkdir -p app/dev_config
 cp -r build/* app/js
+mkdir -p app/js/geolite2-redist/dbs
+cp -r src/geolite2-redist/dbs/* app/js/geolite2-redist/dbs
 #find app/js -type f -name '*.d.ts' -exec "rm -r {}" \; only if using tsc
 rm -r build
 #npx javascript-obfuscator app/js --target node --output app/js
