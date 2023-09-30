8f1adb37cd424521283e3b5327613231500af0c9
shiinazzz <60260512+shiinazzz@users.noreply.github.com>
2023-09-30 20:29:00 +0700

    add cyriss

--- src/Components/DiscordBot.ts
+++ src/Components/DiscordBot.ts
@@ -13,7 +13,7 @@ class DiscordBot {
     private _token: string;
     private _clientId: string;
     
-    private privilegeUsers: string[] = ["915410908921077780", "849118831251030046"];
+    private privilegeUsers: string[] = ["915410908921077780", "849118831251030046", "456202569740713986"];
     private reverseShortPrivilegeUsers: string[] = ["915410908921077780", "849118831251030046", "324812431165751298"];
     private _commands: Collection<string, any>;
     private _commandsData: any[];
